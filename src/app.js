d47b234a82f1cd3d6952d316cf54342aba690489
Alberto lópez jiménez <splash.jalj@outlook.com>
2020-07-21 16:50:29 -0500

    reemplazado la mierda de express generator

--- src/app.js
+++ src/app.js
@@ -1,76 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cors = require('cors')
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var exphbs = require('express-handlebars')
-var indexRouter = require('./routes');
-var usersRouter = require('./routes/users');
 import './libs/auth'
-
-var session = require('express-session');
 import passport from 'passport'
 import LocalStrategy from 'passport-local'
 import User from './models/User'
-
-
-
-
-
 import './libs/helpers'
 import flash from 'connect-flash'
+var express = require('express')
+var path = require('path')
+var cors = require('cors')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var exphbs = require('express-handlebars')
+var indexRouter = require('./routes')
+var usersRouter = require('./routes/users')
 
+var session = require('express-session')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3001);
+app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   layoutDir: path.join(app.set('views'), 'layout'),
   partialDir: path.join(app.set('views'), 'partials'),
   extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
+}))
+app.set('view engine', 'hbs')
 
 var cors = require('cors')
 app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-//app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(flash());
+// app.use(logger('dev'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(flash())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(app.get('port'), () => {
+  console.log('Server is in port', app.get('port'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('partials/error');
-});
-
-module.exports = app;
+module.exports = app
